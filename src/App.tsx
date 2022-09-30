82fda87ca3e3ffbeb90f56703575608bd93e06d7
davelavelle <40757694+davelavelle@users.noreply.github.com>
2022-09-30 14:53:30 +0100

    Add edit feature

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from 'react';
 //@ts-ignore
 import Speech from 'react-speech';
 import './App.css';
+import { Draggable } from 'react-drag-reorder';
 
 function App() {
   const [words, setWords] = useState<string[]>(localStorage.getItem('spellings')?.split(',') ?? [])
@@ -38,8 +39,26 @@ function App() {
     setWord(words[0]);
   }
 
+  const deleteWord = (index : number) => {
+    let savedWords = localStorage.getItem('spellings') ?? '';
+    const savedWordsArray = savedWords.split(',');
+    savedWordsArray.splice(index, 1);
+    localStorage.setItem('spellings', savedWordsArray.join(','));
+    setWords(savedWordsArray);
+
+  }
+
   const listOfWords = words.map((word) =>
-    <li className='list-group-item' key={words.indexOf(word)}>{word}</li>
+    <div className='input-group' key={words.indexOf(word)}>
+      <span className='input-group-text w-5 text-right'>{words.indexOf(word) + 1}.</span>
+      <input className='form-control' type='text' value={word} disabled />
+      {pageStatus === 'editspellings' &&
+        <button className='btn btn-outline-danger' type='submit' onClick={() => deleteWord(words.indexOf(word))} ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"></path>
+          <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"></path>
+        </svg> Delete</button>
+      }
+    </div>
   );
 
   const updateWord = useCallback((e: React.ChangeEvent<HTMLInputElement>, word: string) => {
@@ -58,8 +77,9 @@ function App() {
   }, [updateWord, words]);
 
   const newList = () => {
-    const savedWords = ''
-    localStorage.setItem('spellings', savedWords);
+    // const savedWords = ''
+    // localStorage.setItem('spellings', savedWords);
+    const savedWords = localStorage.getItem('spellings') ?? '';
     setWords(savedWords.split(','));
     setNewWord('');
     setPageStatus('editspellings');
@@ -67,6 +87,9 @@ function App() {
 
   const addWord = (e: React.FormEvent) => {
     e.preventDefault();
+    if (!newWord || newWord.length === 0) {
+      return;
+    }
     let savedWords = localStorage.getItem('spellings') ?? '';
     if (savedWords?.length > 0) {
       savedWords += ',';
@@ -77,6 +100,17 @@ function App() {
     setNewWord('');
 
   }
+
+  const getChangedPos = (currentPos: number, newPos: number) => {
+    console.log(currentPos, newPos);
+    let savedWords = localStorage.getItem('spellings') ?? '';
+    const savedWordsArray = savedWords.split(',');
+    savedWordsArray.splice(newPos, 0, savedWordsArray.splice(currentPos, 1)[0]);
+    localStorage.setItem('spellings', savedWordsArray.join(','));
+    setWords(savedWordsArray);
+
+
+  };
 
   useEffect(() => {
 
@@ -100,7 +134,7 @@ function App() {
                 <input className='btn btn-primary' type='button' value="VIEW SPELLINGS" onClick={() => setPageStatus('viewspellings')} />
               </>
             }
-            <input className='btn btn-primary' type='button' value="NEW LIST" onClick={() => newList()} />
+            <input className='btn btn-primary' type='button' value="EDIT LIST" onClick={() => newList()} />
 
           </div>
         </>
@@ -124,24 +158,35 @@ function App() {
 
       {pageStatus === 'viewspellings' &&
         <div className='d-grid gap-2'>
-          <ol className='list-group list-group-numbered'>
-            {listOfWords}
-          </ol>
+          <div className='gap-0'>
+            <Draggable onPosChange={getChangedPos}>
+              {/* <ol className='list-group list-group-numbered'> */}
+              {listOfWords}
+              {/* </ol> */}
+            </Draggable>
+          </div>
           <input className='btn btn-primary' type='button' value="HOME" onClick={clickEnd} />
         </div>
       }
 
       {pageStatus === 'editspellings' &&
         <>
-          {(words.length > 0 && !(words.length === 1 && words[0] === '')) &&
-            <ol className='list-group list-group-numbered'>
-              {listOfWords}
-            </ol>
-          }
           <div className='d-grid gap-2'>
-            <form className="input-group" onSubmit={addWord} >
+            {(words.length > 0 && !(words.length === 1 && words[0] === '')) &&
+              <div className='d-grid'>
+                {listOfWords}
+              </div>
+            }
+            <form id='form1' className="input-group" onSubmit={addWord} >
               <input type="text" value={newWord} className="form-control" onChange={(e) => { setNewWord(e.target.value) }} />
-              <input className='btn btn-primary' type='submit' value="ADD" />
+              {/* <input className='btn btn-primary' type='submit' value="ADD" /> */}
+              <button className='btn btn-primary' form='form1' type='submit' >
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
+                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"></path>
+                </svg>&nbsp;
+                ADD</button>
+
             </form>
             <input className='btn btn-primary' type='button' value="BACK" onClick={() => setPageStatus('start')} />
           </div>
